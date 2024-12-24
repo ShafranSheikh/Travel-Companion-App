c017a7fe19440d1a5560a345a319c31d876eb617
Mohamed Shafran <shafransheikh@gmail.com>
2024-12-24 21:20:37 +0530

    Implemented Ui fro login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-import '../assets/styles/login.css'
+import '../assets/styles/login.css';
+import googleLogo from '../assets/images/google-logo.png'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login-form-container'>
+      <div className="login-form">
+        <div className="login-header-container">
+          <h2>Login</h2>
+        </div>
+        <form action="">
+          <div className="login-inputs">
+            <label htmlFor="">Email Address</label>
+            <input type="email" name="email" id="email"  required/>
+          </div>
+          <div className="login-inputs">
+            <label htmlFor="">Password</label>
+            <input type="password" name="password" id="password" required/>
+          </div>
+          <div className="login-submit">
+            <button type="submit">Login</button>
+          </div>
+          <div className="login-google">
+            <p>Don't Have an account? &nbsp;<span>signup</span></p>
+            <button><img src={googleLogo} alt="" /></button>
+            <p>Login With Google</p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
