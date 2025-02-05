52fdda2e3b28a4c3e4b670fdb4baa6466b8df54f
Mohamed Shafran <shafransheikh@gmail.com>
2025-02-05 17:29:29 +0530

    Footer section ui completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import '../assets/styles/footer.css'
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    <div className='footer-container'>Footer</div>
+    <div className='footer-container'>
+        <ul>
+          <Link to='/'><li><span>Home</span></li></Link>
+          <Link to='/about'><li>About</li></Link>
+          <Link to='/contact'><li>Contact</li></Link>
+          <Link to='/gallery'><li>Gallery</li></Link>
+          <Link to='/travel'><li>Travel</li></Link>
+        </ul>
+    </div>
   )
 }
 
